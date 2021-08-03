2fa490256c5ced9945a0d98a1352e9655203fb4e
RajatTiniyo <86479367+RajatTiniyo@users.noreply.github.com>
2021-08-03 22:23:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,59 @@
+# voices
+male = "man"
+female = "woman"
+
+# language
+# united state
+united = "en-US"
+# Danish, Denmark
+denmark = "da-DK"
+# German, Germany
+germany = "de-DE"
+# English, Australia
+australia = "en-AU"
+# English, Canada
+canada = "en-CA"
+# English, UK
+uk = "en-GB"
+# English, India
+indiae = "en-IN"
+# English, United States
+unitedstate = "en-US"
+# Catalan, Spain
+spainca = "ca-ES"
+# Spanish, Spain
+spaines = "es-ES"
+# Spanish, Mexico
+maxico = "es-MX"
+# Finnish, Finland
+finland = "fi-FI"
+# French, Canada
+canadafr = "fr-CA"
+# French, France
+france = "fr-FR"
+# Italian, Italy
+italy = "it-IT"
+# Japanese, Japan
+japan = "ja-JP"
+# Korean, Korea
+korea = "ko-KR"
+# Norwegian, Norway
+norway = "nb-NO"
+# Dutch, Netherlands
+netherlands = "nl-NL"
+# Polish-Poland
+poland = "pl-PL"
+# Portuguese, Brazil
+brazil = "pt-BR"
+# Portuguese, Portugal
+portugal = "pt-PT"
+# Russian, Russia
+russia = "ru-RU"
+# Swedish, Sweden
+sweden = "sv-SE"
+# Chinese (Mandarin)
+mandarin = "zh-CN"
+# Chinese (Cantonese)
+cantonese = "zh-HK"
+# Chinese (Taiwanese Mandarin)
+taiwanese = "zh-TW"
